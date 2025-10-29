22c69ea95926589ce333f0d5bfec0ab0f5a503b3
S Koteswararao <srkotesh23@gmail.com>
2025-10-29 15:33:08 +0530

    update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,8 +144,12 @@ CORS_ALLOW_ALL_ORIGINS = True  # only for quick dev; remove in production
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
